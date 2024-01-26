3c61b34a47d3ba249793547b999004577f906736
Danushree2004 <shreedanu2004@gmail.com>
2024-01-26 16:16:19 +0530

    version1

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import Service from './Service'
+import Callback from './Callback'
 
 export default function Home() {
   return (
     <div>
-      <Service/>
+      <Service/><Callback/>
     </div>
   )
 }
